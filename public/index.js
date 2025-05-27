7e331aecdd2b17690d0f2ead8e219282f7c4dbab
Tommy Brits <tommybrits74@gmail.com>
2025-05-27 16:44:51 -0700

    fixed order that cards are dealed to players

--- public/index.js
+++ public/index.js
@@ -1,7 +1,4 @@
-
-
 // Using arrays so I can use Math.random() to index points and card images.
-
 const points = [0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 const imgs = ["./deck/back.png", "./deck/ace_of_clubs.png", "./deck/ace_of_diamonds.png", "./deck/ace_of_hearts.png", "./deck/ace_of_spades.png", "./deck/2_of_clubs.png", "./deck/2_of_diamonds.png", "./deck/2_of_hearts.png", "./deck/2_of_spades.png", "./deck/3_of_clubs.png", "./deck/3_of_diamonds.png", "./deck/3_of_hearts.png", "./deck/3_of_spades.png", "./deck/4_of_clubs.png", "./deck/4_of_diamonds.png", "./deck/4_of_hearts.png", "./deck/4_of_spades.png", "./deck/5_of_clubs.png", "./deck/5_of_diamonds.png", "./deck/5_of_hearts.png", "./deck/5_of_spades.png", "./deck/6_of_clubs.png", "./deck/6_of_diamonds.png", "./deck/6_of_hearts.png", "./deck/6_of_spades.png", "./deck/7_of_clubs.png", "./deck/7_of_diamonds.png", "./deck/7_of_hearts.png", "./deck/7_of_spades.png", "./deck/8_of_clubs.png", "./deck/8_of_diamonds.png", "./deck/8_of_hearts.png", "./deck/8_of_spades.png", "./deck/9_of_clubs.png", "./deck/9_of_diamonds.png", "./deck/9_of_hearts.png", "./deck/9_of_spades.png", "./deck/10_of_clubs.png", "./deck/10_of_diamonds.png", "./deck/10_of_hearts.png", "./deck/10_of_spades.png", "./deck/jack_of_clubs.png", "./deck/jack_of_diamonds.png", "./deck/jack_of_hearts.png", "./deck/jack_of_spades.png", "./deck/queen_of_diamonds.png", "./deck/queen_of_hearts.png", "./deck/queen_of_spades.png", "./deck/queen_of_clubs.png", "./deck/king_of_clubs.png", "./deck/king_of_diamonds.png", "./deck/king_of_hearts.png", "./deck/king_of_spades.png"]
 
@@ -30,29 +27,36 @@ let user = {
     name: "user",
     hand: [],
     cards: [],
-    wallet: 0,
-    bet: 0,
     ace: false,
     score: 0,
     handDisp: document.getElementById("userHand"),
-    betCont: document.querySelector("#betCont"),
     scoreDisp: document.getElementById("userScoreDisp")
 }
+const betCont = document.querySelector("#betCont")
 const betDisp = document.querySelector("#betDisp")
 const walletDisp = document.querySelector("#walletDisp")
 let wallet = 0
 let bet = 0
+
+
 async function getFunds() {
     const funds = await fetch("http://localhost:5000/money")
     const json = await funds.json()
     wallet = await json.money
-    console.log(wallet)
-    walletDisp.innerText = wallet -= 10
-    betDisp.innerText = bet += 10
+    wallet -= 10
+    bet += 10
+    walletDisp.innerText = wallet
+    moneyAmountDisp.innerText = wallet
+    betAmountDisp.innerText = bet
+    betDisp.innerText = bet
 }
 getFunds()
-moneyAmountDisp.innerText = wallet;
-betAmountDisp.innerText = bet;
+
+
+
+
+
+
 async function postFunds() {
     console.log(wallet)
     const tmp = wallet + bet
@@ -63,13 +67,13 @@ async function postFunds() {
         },
         body: `${tmp}`
     }
-    let result 
+    let result
     fetch("http://localhost:5000/money", options)
-    .then(res => {
-        console.log(res.data)
-        result = res.data
-    })
-    .catch(err => console.log(err))
+        .then(res => {
+            console.log(res.data)
+            result = res.data
+        })
+        .catch(err => console.log(err))
 }
 
 
@@ -77,7 +81,6 @@ let dealer = {
     name: "dealer",
     score: 0,
     hand: [],
-    cards: [],
     ace: false,
     playing: false,
     handDisp: document.getElementById("dealerHand"),
@@ -88,17 +91,23 @@ let table = []
 // Functions to bring everything to their original values except winnings
 function resetHands() {
     table = []
-    dealer = {...dealer, hand: [], score: 0, cards: [], ace: false, playing: false}
-    user = {...user, hand: [], cards: [], ace: false, score: 0}
+    user.score = 0
+    user.hand = []
+    user.ace = false
+    user.playing = false
+    dealer.score = 0
+    dealer.hand = []
+    dealer.ace = false
+    dealer.playing = false
     user.scoreDisp.innerText = user.score
     dealer.scoreDisp.innerText = dealer.score
 }
 
 function resetDOM(player) {
-    player.cards.map(card => {
-        player.handDisp.removeChild(card)
-    })  
-    player.cards = []  
+    const arr = Array.from(player.handDisp.children)
+    for (let i = 0; i < arr.length; i++) {
+        player.handDisp.removeChild(arr[i])
+    }    
 }
 
 
@@ -111,7 +120,7 @@ function winnings(result) {
         bet = 0
         betDisp.innerText = bet
     }
-    
+
 }
 
 
@@ -129,7 +138,7 @@ function winDisplay(result) {
     } else if (result === "tie") {
         resultsDisp.innerText = "Draw"
     }
-    
+
 }
 
 
@@ -139,6 +148,8 @@ function updateBet() {
     walletDisp.innerText = wallet
 }
 updateBet()
+
+
 function hidePlayBtns() {
     const { playBtnsCont } = btns
     playBtnsCont.style.display = "none"
@@ -147,18 +158,21 @@ function showPlayBtns() {
     const { playBtnsCont } = btns
     playBtnsCont.style.display = "flex"
 }
+
+
+
 hidePlayBtns()
+
+
 function hideBet() {
     if (!betCont.classList.contains("hidden")) {
         betCont.classList.add("hidden")
         scoresCont.classList.remove("hidden")
-
     } else {
         scoresCont.classList.add("hidden")
         betCont.classList.remove("hidden")
     }
 }
-
 
 // To generate a random number that isn't arleady in table array
 // then adding new index to the table array to make sure it isn't called again.
@@ -183,27 +197,44 @@ function createCard(player, num) {
     img.classList.add("card-img")
     if (player.name === "dealer") {
         cont.classList.add("dealer-card-div")
-        cont.style.left = `${Math.abs(player.hand.length * 0.3)}vw`
+        cont.style.left = `${Math.abs(player.hand.length * 1.8)}vw`
     } else {
         cont.classList.add("user-card-div")
+        cont.style.left = `${Math.abs(player.hand.length * 1.8)}vw`
     }
     cont.append(img)
     player.handDisp.append(cont)
 }
 
+function downCard(player) {
+    const cont = document.createElement("div")
+    const img = document.createElement("img")
+    cont.classList.add('dealer-card-div')
+    img.classList.add("card-img")
+    img.src = imgs[0]
+    cont.append(img)
+    cont.style.left = `${Math.abs(player.hand.length * 1.8)}vw`
+    player.handDisp.append(cont)
+}
 
 function dealCard(player) {
     const num = rando()
-    player.hand.push(num)
-    createCard(player, num)
+    if (player.name === 'dealer' && player.playing === false && player.hand.length === 1) {
+        player.hand.push(num)
+        downCard(player)
+    } else {
+        player.hand.push(num)
+        createCard(player, num)
+    }
 }
+
 
 // to calculate score
 
 function calcScore(player) {
     player.score = 0
     if (player.name === "dealer" && player.playing === false) {
-        player.score += points[player.hand[1]]
+        player.score += points[player.hand[0]]
         player.scoreDisp.innerText = player.score
     } else {
         for (let i = 0; i < player.hand.length; i++) {
@@ -244,35 +275,41 @@ function checkAce(player) {
 
 //Opening function to call other child functions for initial deal
 
-function deal() {
+async function deal() {
+    resetDOM(dealer)
+    resetDOM(user)
     postFunds()
     hideBet()
     showPlayBtns()
-    dealCard(dealer)
-    dealCard(user)
-    dealCard(dealer)
-    dealCard(user)
-    createCards(user)
-    createCards(dealer)
-    calcScore(user)
-    calcScore(dealer)
-    checkAce(user)
-    checkAce(dealer)
-    console.log(dealer)
+    setTimeout(() =>    dealCard(dealer), "500")
+    setTimeout(() => dealCard(user), "1000")
+    setTimeout(() => dealCard(dealer), "1500")
+    setTimeout(() => {
+        dealCard(user)
+        calcScore(user)
+        calcScore(dealer)
+        checkAce(user)
+        checkAce(dealer)
+        console.log(dealer)
+    }, 2000)
 }
 
 function stand() {
+    let arr = Array.from(dealer.handDisp.children)
+    btns.playBtnsCont.style.display = "none"
+    dealer.handDisp.removeChild(arr[1])
+    dealer.playing = true
+    createCard(dealer, dealer.hand[1])
     setInterval(() => {
-        if (dealer.score <= 17) {
+        while (dealer.score <= 17) {
             hit(dealer)
-        } else {
+        }
             clearInterval()
-            
             endDisp.style.display = "flex"
             if (dealer.score > user.score && dealer.score <= 21) {
                 winDisplay("lose")
                 winnings("lose")
-            } else if (dealer.score === user.score || (dealer.score > 21 && user.score > 21 )) {
+            } else if (dealer.score === user.score || (dealer.score > 21 && user.score > 21)) {
                 winDisplay("tie")
             } else if (dealer.score < user.score && user.score <= 21) {
                 winDisplay("win")
@@ -284,7 +321,7 @@ function stand() {
                 winDisplay("lose")
                 winnings("lose")
             }
-        }
+        
     }, 800)
 }
 
@@ -302,7 +339,6 @@ function hit(player) {
         player.playing = true
     }
     dealCard(player)
-    createCards(player)
     calcScore(player)
     checkAce(player)
     checkBust(player)
@@ -311,7 +347,7 @@ function hit(player) {
 function betUp() {
     if (wallet > 10) {
         wallet -= 10
-        bet += 10        
+        bet += 10
         updateBet()
     }
 }
@@ -333,22 +369,10 @@ btns.bet.map((btn, idx) => {
     })
 })
 
+
+
+
+
 btns.deal.addEventListener("click", () => deal())
 btns.hit.addEventListener("click", () => hit(user))
 btns.stand.addEventListener("click", () => stand())
-btns.sameBtn.addEventListener("click", () => {
-    endDisp.style.display = "none"
-    resetDOM(dealer)
-    resetDOM(user)
-    resetHands()
-    hideBet()
-    deal()
-})
-btns.newBtn.addEventListener("click", () => {
-    endDisp.style.display="none"
-    resetDOM(dealer)
-    resetDOM(user)
-    resetHands()
-    hidePlayBtns()
-    btns.betBtnsCont.style.display = "flex"  
-})
